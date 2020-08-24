c2cc9d5d34f891d2356e0aa3c849d9df86243f56
Nugine <nugine@foxmail.com>
2020-08-24 13:54:39 +0800

    add AlignedBox

---
+++ src/boxed.rs
@@ -0,0 +1,115 @@
+use crate::aligned_alloc;
+
+use alloc::alloc::{alloc, dealloc, Layout};
+use core::fmt::{self, Debug};
+use core::marker::PhantomData;
+use core::mem;
+use core::ops::{Deref, DerefMut};
+use core::ptr::{drop_in_place, NonNull};
+
+/// Aligned heap allocation
+pub struct AlignedBox<T: ?Sized> {
+    inner: NonNull<T>,
+    align: usize,
+    _marker: PhantomData<T>,
+}
+
+unsafe impl<T: ?Sized + Send> Send for AlignedBox<T> {}
+unsafe impl<T: ?Sized + Sync> Sync for AlignedBox<T> {}
+
+#[cfg(feature = "std")]
+mod std_impl {
+    use super::AlignedBox;
+
+    use std::panic::{RefUnwindSafe, UnwindSafe};
+
+    impl<T: ?Sized + UnwindSafe> UnwindSafe for AlignedBox<T> {}
+    impl<T: ?Sized + RefUnwindSafe> RefUnwindSafe for AlignedBox<T> {}
+}
+
+impl<T> AlignedBox<T> {
+    /// Allocates memory on the heap with specified alignment and then places x into it.
+    ///
+    /// This doesn't actually allocate if T is zero-sized.
+    pub fn new(x: T, align: usize) -> Self {
+        let size = mem::size_of::<T>();
+        let align = mem::align_of::<T>().max(align);
+
+        let inner = if size == 0 {
+            NonNull::dangling()
+        } else {
+            unsafe {
+                let ptr = aligned_alloc(alloc, size, align).cast::<T>();
+                ptr.write(x);
+                NonNull::new_unchecked(ptr)
+            }
+        };
+
+        Self {
+            align,
+            inner,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<T: ?Sized> AlignedBox<T> {
+    /// Consumes the box, returning a raw pointer and alignment.
+    pub fn into_raw(this: Self) -> (*mut T, usize) {
+        let ans = (this.inner.as_ptr(), this.align);
+        mem::forget(this);
+        ans
+    }
+
+    /// Constructs a aligned box from a raw pointer and alignment
+    /// # Safety
+    /// + `ptr` must be non-null and well aligned for `align`
+    /// + `ptr` must be allocated with the global allocator
+    pub unsafe fn from_raw(ptr: *mut T, align: usize) -> Self {
+        Self {
+            inner: NonNull::new_unchecked(ptr),
+            align,
+            _marker: PhantomData,
+        }
+    }
+
+    /// Returns the alignment of the box
+    pub fn alignment(this: &Self) -> usize {
+        this.align
+    }
+}
+
+impl<T: ?Sized> Drop for AlignedBox<T> {
+    fn drop(&mut self) {
+        unsafe {
+            let size = mem::size_of_val(self.inner.as_ref());
+            if size == 0 {
+                return;
+            }
+            let ptr = self.inner.as_ptr();
+            drop_in_place(ptr);
+
+            let layout = Layout::from_size_align_unchecked(size, self.align);
+            dealloc(ptr.cast(), layout)
+        }
+    }
+}
+
+impl<T: ?Sized> Deref for AlignedBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.inner.as_ptr() }
+    }
+}
+
+impl<T: ?Sized> DerefMut for AlignedBox<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.inner.as_ptr() }
+    }
+}
+
+impl<T: ?Sized + Debug> Debug for AlignedBox<T> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        T::fmt(self, f)
+    }
+}
---
+++ src/bytes.rs
@@ -0,0 +1,109 @@
+use crate::aligned_alloc;
+use crate::boxed::AlignedBox;
+
+use alloc::alloc::alloc_zeroed;
+use core::fmt::{self, Debug};
+use core::ops::{Deref, DerefMut};
+use core::slice;
+
+/// A continuous fixed-length byte array with a specified alignment.
+pub struct AlignedBytes {
+    inner: AlignedBox<[u8]>,
+}
+
+impl AlignedBytes {
+    /// Allocate a zero-initialized byte array with an exact alignment.
+    pub fn new_zeroed(len: usize, align: usize) -> Self {
+        let inner = unsafe {
+            let ptr = aligned_alloc(alloc_zeroed, len, align);
+            AlignedBox::from_raw(slice::from_raw_parts_mut(ptr, len), align)
+        };
+        Self { inner }
+    }
+
+    /// Returns the alignment of the byte array.
+    pub fn alignment(&self) -> usize {
+        AlignedBox::alignment(&self.inner)
+    }
+}
+
+impl Debug for AlignedBytes {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        <[u8] as Debug>::fmt(&**self, f)
+    }
+}
+
+impl Deref for AlignedBytes {
+    type Target = [u8];
+    fn deref(&self) -> &Self::Target {
+        &*self.inner
+    }
+}
+
+impl DerefMut for AlignedBytes {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut *self.inner
+    }
+}
+
+impl AsRef<[u8]> for AlignedBytes {
+    fn as_ref(&self) -> &[u8] {
+        &*self
+    }
+}
+
+impl AsMut<[u8]> for AlignedBytes {
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut *self
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::AlignedBytes;
+
+    #[test]
+    fn check_alignment() {
+        let align = 4096;
+        let bytes = AlignedBytes::new_zeroed(8, align);
+        assert_eq!(bytes.alignment(), align);
+        assert!(bytes.as_ptr() as usize % align == 0);
+    }
+
+    #[should_panic(expected = "Invalid layout: size = 1, align = 0")]
+    #[test]
+    fn check_layout_zero_align() {
+        AlignedBytes::new_zeroed(1, 0);
+    }
+
+    #[should_panic(expected = "Invalid layout: size = 1, align = 3")]
+    #[test]
+    fn check_layout_align_not_power_of_2() {
+        AlignedBytes::new_zeroed(1, 3);
+    }
+
+    #[should_panic]
+    #[test]
+    fn check_layout_overflow() {
+        let size = core::mem::size_of::<usize>() * 8;
+        AlignedBytes::new_zeroed((1usize << (size - 1)) + 1, 1usize << (size - 1));
+    }
+
+    #[test]
+    fn check_markers() {
+        fn require_send<T: Send>() {}
+        fn require_sync<T: Sync>() {}
+
+        require_send::<AlignedBytes>();
+        require_sync::<AlignedBytes>();
+
+        #[cfg(feature = "std")]
+        {
+            use std::panic::{RefUnwindSafe, UnwindSafe};
+            fn require_unwind_safe<T: UnwindSafe>() {}
+            fn require_ref_unwind_safe<T: RefUnwindSafe>() {}
+            require_unwind_safe::<AlignedBytes>();
+            require_ref_unwind_safe::<AlignedBytes>();
+        }
+    }
+}
